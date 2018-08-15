e266cb5c4faa4c0417548c8f49f51c75bafa3696
Tania Munoz <tania.m.jimenez24@gmail.com>
2018-08-14 23:14:07 -0500

    menu links

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   
   root 'main_page#home'
-  get 'main_page/work'
-  get 'main_page/services'
-  get 'main_page/blog'
-  get 'main_page/contact'
+
+ 
+  get  '/work', to: 'main_page#work'
+  get  '/services', to: 'main_page#services'
+  get  '/blog', to: 'main_page#blog'
+  get  '/contact', to: 'main_page#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
